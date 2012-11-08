d938e42deb32903456dc30a89c0d95a94f52af68
Zachary Bush <zach@zmbush.com>
2012-11-07 19:39:30 -0800

    Add beginnings of unit tests

---
+++ test_gamesman.rb
@@ -0,0 +1,17 @@
+require 'gamesman'
+require 'test/unit'
+require 'rack/test'
+
+ENV['RACK_ENV'] = 'test'
+
+class HelloWorldTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Gamesman
+  end
+
+  def test_true
+    assert true
+  end
+end
