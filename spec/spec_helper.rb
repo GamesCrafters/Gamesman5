429e9863f69e4cdf429c463e21cc1900f9f08a7e
Zachary Bush <zmbush@berkeley.edu>
2013-02-21 12:16:03 -0800

    Use webkit without headless gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,13 +16,13 @@ Capybara.app = Gamesman.new
 Capybara.javascript_driver = :webkit
 Capybara.default_wait_time = 1
 
-if Capybara.javascript_driver == :webkit
-  require 'headless'
-
-  headless = Headless.new
-  headless.start
-
-  at_exit do
-    headless.destroy
-  end
-end
+#if Capybara.javascript_driver == :webkit
+#  require 'headless'
+#
+#  headless = Headless.new
+#  headless.start
+#
+#  at_exit do
+#    headless.destroy
+#  end
+#end
