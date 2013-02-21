e90a6056addf10f4828079b2cb64d9fef7e858da
Zachary Bush <zmbush@berkeley.edu>
2013-02-21 11:58:21 -0800

    Fix asset tests to use assetpack

--- spec/loaders_spec.rb
+++ spec/loaders_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe "Loaders" do
   it "should render coffeescript" do
-    File.open('assets/javascripts/test.js.coffee', 'w') do |f|
+    File.open('assets/javascripts/test.coffee', 'w') do |f|
       f.write('number = -42 if false')
     end
-    visit '/javascripts/test.js'
+    visit '/js/test.js'
     page.should have_text "(function() { var number; if (false) { number = -42; } }).call(this);"
-    File.delete('assets/javascripts/test.js.coffee')
+    File.delete('assets/javascripts/test.coffee')
   end
 
   it "should render raw javascript" do
     File.open('assets/javascripts/test.js', 'w') do |f|
       f.write('number = -42;')
     end
-    visit '/javascripts/test.js'
+    visit '/js/test.js'
     page.should have_text "number = -42;"
     File.delete('assets/javascripts/test.js')
   end
